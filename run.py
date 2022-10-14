b409c3ffaeab122be0d4f507495e79180746e68c
Christoph33one <cjbrowning23@hotmail.com>
2022-10-14 15:13:06 +0000

    intital commit

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+QUESTIONS = [
+    {
+        'question': 'Is there a volcano on Mars?',
+        'option': 'A. yes''B. no',
+        'correctAnswer': 'A.'
+    },
+    {
+        'question': 'How many moons are in our solar system?',
+        'option': 'A. 100 moons' 'B. 150 moons' 'C. 181 moons',
+        'correctAnswer': 'C.' 
+    },
+    {
+    
+        'question': 'Which is the brightest planet in the night sky?',
+        'option': 'A. Mars' 'B. Venus' 'C. Neptune',
+        'correctAnswer': 'B.'
+    },
+    ]
+
+
