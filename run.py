14ceea7770c180c459043e562e0566fedc8e4109
Christoph33one <cjbrowning23@hotmail.com>
2022-10-15 11:39:38 +0000

    Create a list of questions and a 2d dictionary of answers

--- run.py
+++ run.py
@@ -1,20 +1,21 @@
-QUESTIONS = [
-    {
-        'question': 'Is there a volcano on Mars?',
-        'option': 'A. yes''B. no',
-        'correctAnswer': 'A.'
-    },
-    {
-        'question': 'How many moons are in our solar system?',
-        'option': 'A. 100 moons' 'B. 150 moons' 'C. 181 moons',
-        'correctAnswer': 'C.' 
-    },
-    {
-    
-        'question': 'Which is the brightest planet in the night sky?',
-        'option': 'A. Mars' 'B. Venus' 'C. Neptune',
-        'correctAnswer': 'B.'
-    },
-    ]
+questions = {
+    "How much do NASA space suits cost?: ": "A.",
+    "How many moons are in our solar system?: ": "B.",
+    "Which is the brightest planet in the night sky?: ": "C.",
+    "How long is one year on Jupiter?: ": "A."
+}
 
+options = [
+        ["A. 12 million dollars?", "B. 20 million dollars?"],
+        ["A. 100 moons?", "B. 181 mooons?", "C. 300 moons?"],
+        ["A. Mars?", "B. Neptune?", "C. venus?"],
+        ["A. 12 years?", "6 years?", "C. 8 years?"]
+]
+"""
+show user next question from dictionary of 
+questions.
+Question index incremented by one.
+
+
+"""
 
