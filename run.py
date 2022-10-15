194f33269a025b8bd17e3ea826e9083fcd9e0eb6
Christoph33one <cjbrowning23@hotmail.com>
2022-10-15 13:07:16 +0000

    display input ti the user A B or c,

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ Question index incremented by one.
 
 """
 
-def getnextQuestion():
+def get_next_question():
 
     user_guesses = []
     correct_guesses = 0
@@ -28,25 +28,40 @@ def getnextQuestion():
     for key in questions:
         print("-------")
         print(key)
-        for i in options[question_index]: 
+        for i in options[question_index-1]: 
             print(i)
-            
+       
         question_index += 1
 
 """
-User to enter guess
-User can enter lower case letters
 
+User to enter guess 
 """
-def takeUserGuess():
-    guess = input("Enter(A, B or C:)")
-    guess = guess.lower()
+def take_user_answer():
+    
+    user_guess = input("Enter(A. B. or C.)")
+    user_guess = user_guess.lower()
+    user_guesses.append(user_guess)
+    
+"""
 
+Checks users answer and users guess in the 
+empty user guess variable.
+"""
 
+def check_user_answer(user_guess):
+    if user_guess == user_guesses:
+        print("correct!")
+        return 1
+    else:
+        print("Wrong!")
+        return 0 
+    
 """
 
 Calls all game functions in main function
 """
 if __name__ == '__main__':
-    getnextQuestion()
-    takeUserGuess()
+    get_next_question()
+    take_user_answer()
+    check_user_answer(questions.get(key),user_guess)
