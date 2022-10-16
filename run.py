83030dc2d0ec2cd725d49c57a5e25b4f1f4386c5
Christoph33one <cjbrowning23@hotmail.com>
2022-10-16 19:39:13 +0000

    add check_answer function with if statement. If users guess is equal to correect answer, print correct, else print incorrect

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
+"""
+Print welcome message to the user.
+"""
+print("Welcome to Sapced out!")
+playing = input("Press any key to start! ")
+if playing == playing:
+    print("Lets play!🚀")
+
 def get_next_question():
     """
 
@@ -12,55 +20,55 @@ def get_next_question():
     for key in questions:
         print("-------")
         print(key)
-        for i in options[question_index-1]: 
+        for i in options[question_index-1]:
             print(i)
         guess = input("Enter (A, B, or C):\n ")
         guess = guess.upper()
         guesses.append(guess)
+
         correct_guesses += check_answer(questions.get(key), guess)
-        question_index += 1 
-    display_score(correct_guesses, guesses)
+        question_index += 1
+    display_result(correct_guesses, guesses)
 
-def check_answer(answer, guess):
+def check_answer(answer, guesses):
     """
-
-    Check users answer from the 
-    check answer function, call it in the,
-    get next question function. with parameters (answer ,guess),
+    Check users_answer from the
+    check_answer function, called it in the
+    get next_question function, with parameters (answer ,guess),
     getting the questions KEY, and guess
     if statement to comare if answer and are guees and equal
-    if not "not correct!"
+    if not "not correct!".
     """
-    if answer == guess:
-        print("correct!")
-        return +1
+    if answer == guesses:
+        print("wrong")
+        return True
     else:
-        print("Wrong!")
-        return 0
-        
-def display_score(correct_guesses, guesses):
+        print("correct!")
+        return False
+
+def display_result(correct_guesses, guesses):
     """
-    Takes value from correct guesses variable 
-    and guesses list as parameters. 
-    Displays guesses and answers to the 
+    Takes value from correct guesses variable
+    and guesses list as parameters.
+    Displays guesses and answers to the
     user and the end of the game.
     """
     print("--------------")
     print("RESULT!")
     print("--------------")
-
     print("CORRECT ANSWERS:")
     for i in questions:
         print(questions.get(i))
-    print()
-
+    print("--------------")
     print("YOUR GUESSES:")
     for i in guesses:
         print(i)
-    print()
 
-    score = int((correct_guesses/len(questions))*100)
-    print("YOUR SCORE IS, "+str(score)+"%")
+def end_game():
+    """
+    End of game function
+    """
+    print("End of game!")
 
 questions = {
     "How much do NASA space suits cost?": "A.",
@@ -69,7 +77,6 @@ questions = {
     "How long is one year on Jupiter? ": "A.",
     "Which planet is closest in size to Earth?": "A."
 }
-
 options = [
         ["A. 12 million dollars?", "B. 20 million dollars?"],
         ["A. 100 moons?", "B. 181 mooons?"],
@@ -77,11 +84,11 @@ options = [
         ["A. 12 years?", "B. 6 years?", "C.8 years?"],
         ["A.Mars?", "B.Venus?"]
 ]
-
-"""
-
-Calls all game functions in main function
-"""
-if __name__ == '__main__':
+def main():
     get_next_question()
-    
+    check_answer()
+    display_result()
+    end_game()
+
+if __name__ == '__main__':
+    main()
