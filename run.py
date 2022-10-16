fd3984c76ef3b43f61cd0239e4bdf2e3cb8456dd
Christoph33one <cjbrowning23@hotmail.com>
2022-10-16 13:16:18 +0000

    need to work on adding users score to the game.

--- run.py
+++ run.py
@@ -37,7 +37,7 @@ def check_answer(answer, guess):
     else:
         print("Wrong!")
         return 0
-
+        
 def display_score(correct_guesses, guesses):
     """
     Takes value from correct guesses variable 
